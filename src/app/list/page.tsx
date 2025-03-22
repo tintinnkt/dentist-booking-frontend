eb3025b7e74d99e80acee5fa922c0e6091d14114
Krittin Tragunejindarat <t.tintinnkt@gmail.com>
2025-03-22 15:42:41 +0700

    feat: add get dentist data from backend

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,54 @@
+"use client";
 import DentistCard from "@/components/DentistCard";
 import { SearchBar } from "@/components/SearchBar";
-import { dentists } from "@/mock/DentistsMock";
+import { BackendRoutes } from "@/conifg/apiRoutes";
+import { DentistProps } from "@/types/api/Dentist";
+import axios, { AxiosError } from "axios";
+import { LoaderIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { ErrorIcon } from "react-hot-toast";
 
-const page = () => {
+const Page = () => {
+  const [dentists, setDentists] = useState<DentistProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(BackendRoutes.DENTIST);
+
+        if (Array.isArray(response.data.data)) {
+          setDentists(response.data.data);
+        } else {
+          console.error("Expected an array but got:", response.data);
+        }
+      } catch (err) {
+        const error = err as AxiosError;
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error)
+    return (
+      <p className="text-red-500">
+        <ErrorIcon /> Error: {error}
+      </p>
+    );
+
+  if (loading)
+    return (
+      <p className="text-gray-500">
+        <LoaderIcon /> Loading...
+      </p>
+    );
+
   return (
     <main className="mx-auto my-10 max-w-screen-xl px-8">
       <div className="gap-x-10 md:grid md:grid-cols-11">
@@ -16,9 +62,13 @@ const page = () => {
         <section className="mt-5 sm:col-span-4 lg:col-span-3">hello</section>
         <section className="col-span-7 my-10 flex h-full w-full flex-col justify-between gap-y-4 md:my-0 lg:col-span-8">
           <div className="col-span-8 w-full space-y-6 p-5">
-            {dentists.map((dentist, idx) => (
-              <DentistCard key={idx} dentist={dentist} />
-            ))}
+            {dentists.length > 0 ? (
+              dentists.map((dentist, idx) => (
+                <DentistCard key={idx} dentist={dentist} />
+              ))
+            ) : (
+              <p>No dentists found.</p>
+            )}
           </div>
         </section>
       </div>
@@ -26,4 +76,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
