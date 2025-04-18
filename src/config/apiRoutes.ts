a973601bffa272ce3548cfadd56b1955a67c8491
Supawich Rangponsumrit <UnicornIsReal18@gmail.com>
2025-04-18 22:08:38 +0700

    feat: add base admin page all in one ver

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -1,5 +1,5 @@
-// const BACKEND_URL = "https://project-s-backend-cyan.vercel.app";
-const BACKEND_URL = "http://127.0.0.1:5003";
+const BACKEND_URL = "https://project-s-backend-cyan.vercel.app";
+//const BACKEND_URL = "http://127.0.0.1:5003";
 const BackendRootRoutes = `${BACKEND_URL}/api/v1`;
 
 export enum BackendRoutes {
@@ -8,13 +8,15 @@ export enum BackendRoutes {
   REGISTER = `${BackendRootRoutes}/auth/register`,
   UPDATE_USER = `${BackendRootRoutes}/auth/updateUser`,
   USER_INFO = `${BackendRootRoutes}/auth/me`,
-
   DENTIST = `${BackendRootRoutes}/dentists`,
   BOOKING = `${BackendRootRoutes}/bookings`,
   REGISTER_DENTIST = `${BackendRootRoutes}/auth/registerDent`,
   UNAVAILABLE_BOOKING = `${BackendRootRoutes}/bookings/unavailable`,
   GET_ALL_DENTISTS_SCHEDULES = `${BackendRootRoutes}/bookings/schedules`,
   UNAVAILABLE_BOOKING_BY_DENTIST_ID = `${BackendRootRoutes}/dentists/:dentistId/unavailable`,
+  SCHEDULES = `${BackendRootRoutes}/unavailable`,
+  OFF_HOURS = `${BackendRootRoutes}/offHours`,
+  OFF_HOURS_BY_OWNER = `${BackendRootRoutes}/offHours/owner/:ownerId`
 }
 
 export const getUnavailableBookingByDentistId = (dentistId: string): string =>
