a3d7d31c82f2e445a9a16ddf24e792a0e5633bd3
Supawich Rangponsumrit <UnicornIsReal18@gmail.com>
2025-04-19 19:28:27 +0700

    feat: update component

--- src/components/ui/DatePicker.tsx
+++ src/components/ui/DatePicker.tsx
@@ -52,7 +52,7 @@ export function DatePickerWithPresets({
             !date && "text-muted-foreground",
           )}
         >
-          <CalendarIcon className="mr-2 h-4 w-4" />
+          <CalendarIcon />
           {date ? format(date, "PPP") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
@@ -62,7 +62,7 @@ export function DatePickerWithPresets({
       >
         <Select
           onValueChange={(value) =>
-            handleDateSelect(addDays(new Date(), parseInt(value)))
+            setDate(addDays(new Date(), parseInt(value)))
           }
         >
           <SelectTrigger>
