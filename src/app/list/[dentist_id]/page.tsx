65c206a08a4669eaaebce8c57626bcc47099f024
Krittin Tragunejindarat <t.tintinnkt@gmail.com>
2025-04-23 22:44:34 +0700

    fix: add comment mock data in dentist detail page

--- src/app/list/[dentist_id]/page.tsx
+++ src/app/list/[dentist_id]/page.tsx
@@ -16,8 +16,17 @@ interface Comment {
   comment_id: string;
   message: string;
 }
+interface OffHour {
+  owner: string;
+  _id: string;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+  isForAllDentist: boolean;
+}
 interface DentistDetailsProps extends DentistProps {
-  comments: Array<Comment>;
+  comments?: Array<Comment>;
+  offHour?: OffHour[];
 }
 const DentistDetailsMock: DentistDetailsProps = {
   id: "001",
@@ -27,6 +36,25 @@ const DentistDetailsMock: DentistDetailsProps = {
   },
   yearsOfExperience: 1,
   areaOfExpertise: ["Orthodontics", "Periodontics", "Oral Surgery"],
+  offHour: [
+    {
+      _id: "001",
+      owner: "121",
+      startDate: new Date(),
+      endDate: new Date(),
+      description: "SongKran",
+      isForAllDentist: true,
+    },
+    {
+      _id: "002",
+      owner: "121",
+      startDate: new Date(),
+      endDate: new Date(),
+      description: "",
+      isForAllDentist: false,
+    },
+  ],
+
   comments: [
     {
       comment_id: "001",
@@ -38,6 +66,17 @@ const DentistDetailsMock: DentistDetailsProps = {
 
 const DentistDetails = () => {
   const router = useRouter();
+  // const { dentist_id } = useParams<{ dentist_id: string }>();
+  // const {
+  //   data: DentistDetailsProps,
+  //   isLoading,
+  //   error,
+  // } = useQuery({
+  //   queryKey: ["DENTIST_DETAIL", dentist_id],
+  //   queryFn: async () => {
+  //     await axios.get(BackendRoutes.DENTIST + `/${dentist_id}`);
+  //   },
+  // });
   return (
     <main className="mx-auto max-w-screen-xl space-y-4 gap-x-5 md:grid-cols-10 md:space-y-0 md:pt-4 lg:grid lg:py-8">
       <section className="px-3 pb-2 md:col-span-1 md:pb-4 lg:pb-0">
@@ -48,7 +87,7 @@ const DentistDetails = () => {
           }}
         />
       </section>
-      <section className="m-1 flex items-center justify-between md:col-span-9">
+      <section className="m-1 flex flex-col items-center justify-between space-y-2 md:col-span-9">
         <Card className="w-full rounded-3xl bg-white p-5 shadow-md shadow-neutral-200 sm:p-8">
           <CardHeader>
             <CardTitle className="text-4xl">
@@ -68,19 +107,18 @@ const DentistDetails = () => {
             <CardFooter className="flex justify-end space-x-3">
               <CustomButton useFor="edit" />
               <CustomButton useFor="delete-dentist" />
-              {/* <CustomButton
-                      hideTextOnMobile={true}
-                      useFor="add-booking-section"
-                      onClick={handleAppointmentBooking}
-                      disabled={!appDate || !appTime || isCreating}
-                      className="w-full"
-                      isLoading={isCreating}
-                    >
-                      {isCreating ? "Booking..." : "Book Appointment"}
-                    </CustomButton> */}
             </CardFooter>
           </CardHeader>
         </Card>
+        <h2>Unavalible Date</h2>
+        <ul className="w-full max-w-md list-disc">
+          {DentistDetailsMock.offHour?.map((offHour, index) => (
+            <li key={index} className="flex w-full space-x-3">
+              <div>{offHour.startDate.toDateString()}</div>
+              <div>{offHour.description}</div>
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
