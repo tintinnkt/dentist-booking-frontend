b3c3a8c7f9b973a3c55f206daeb31ff4fb08301e
MasterNay <155442296+MasterNay@users.noreply.github.com>
2025-04-24 20:11:46 +0700

    fix date and sort schedule

--- src/components/ScheduleManament.tsx
+++ src/components/ScheduleManament.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { getDentistSchedule } from "@/lib/dentist/getDentistSchedule";
 import { Booking } from "@/types/api/Dentist";
 import { useSession } from "next-auth/react";
+import { LoaderIcon } from "lucide-react";
 
 export default function ScheduleManagement() {
   const [selectedDate, setSelectedDate] = useState("");
@@ -34,7 +35,6 @@ export default function ScheduleManagement() {
 
       if (data.length === 0) {
         setMessage("ไม่พบตารางนัดหมายของทันตแพทย์ในวันที่เลือก");
-        console.log("AAAAAAAAAAAAA")
       } else {
         setMessage("");
         setShow(true);
@@ -73,42 +73,48 @@ export default function ScheduleManagement() {
         </div>
         
           {loading && (
-            <div className="mt-4 text-blue-500 font-semibold">Loading...</div>
+            <div className="flex items-center justify-center gap-3 pt-10">
+            <LoaderIcon /> Loading schedules...
+          </div>
           )}
 
-        {message && (
-          <div className="mt-4 text-red-600 font-semibold">{message}</div>
-        )}
-        {show && schedules.length > 0 && (
-          <div className="mt-4 text-sm text-black font-bold">
-            Schedule on {selectedDate || "this day"}
-            {schedules.map((item) => {
-              const apptDate = new Date(item.apptDateAndTime);
+          {message && (
+            <div className="mt-4 text-red-600 font-semibold">{message}</div>
+          )}
+          {show && schedules.length > 0 && (
+            <div className="mt-4 text-sm text-black font-bold">
+              Schedule on {selectedDate || "this day"}
+              {schedules
+                .sort((a, b) => new Date(a.apptDateAndTime).getTime() - new Date(b.apptDateAndTime).getTime())
+                .map((item) => {
+                const apptDate = new Date(item.apptDateAndTime);
 
-              if (isNaN(apptDate.getTime())) {
-                console.warn("Invalid date:", item.apptDateAndTime);
-                return null; // หรือจะแสดง error ก็ได้
-              }
+                if (isNaN(apptDate.getTime())) {
+                  console.warn("Invalid date:", item.apptDateAndTime);
+                  return null; // หรือจะแสดง error ก็ได้
+                }
             
-              const bookingDate = apptDate.toISOString().split("T")[0];
-              if (bookingDate !== selectedDate) return null;
+                const bookingDate = apptDate.toISOString().split("T")[0];
 
-              if (!item.dentist) return null;
-        return (
-          <div key={item._id} className="mt-4">
-            <h3 className="text-blue-600 text-xl">{item.user.name}</h3>
-            <ul className="list-disc ml-5 text-gray-700">
-              <li>
-                <strong>User ID:</strong> {item.user._id} <br />
-                <strong>Appointment:</strong> {new Date(item.apptDateAndTime).toLocaleString()}<br />
-                <strong>Status:</strong> {item.status}
-              </li>
-          </ul>
-        </div>
-      );
-    })}
-  </div>
-)}
+                if (bookingDate !== selectedDate && selectedDate !== "") 
+                  return null;
+
+                if (!item.dentist) return null;
+                return (
+                  <div key={item._id} className="mt-4">
+                    <h3 className="text-blue-600 text-xl">{item.user.name}</h3>
+                    <ul className="list-disc ml-5 text-gray-700">
+                      <li>
+                        <strong>User ID:</strong> {item.user._id} <br />
+                        <strong>Appointment:</strong> {new Date(item.apptDateAndTime).toUTCString()}<br />
+                        <strong>Status:</strong> {item.status}
+                      </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
